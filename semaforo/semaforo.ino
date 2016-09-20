c1c0fa6e312d49e2101654b690fce6f453413212
Wendell Rocha <wendellrocha@outlook.com>
2016-09-19 22:31:16 -0300

    Update

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -1,17 +1,17 @@
-/* Desafio do semáforo com semáforo para padestres                      *
+/* Desafio do sem?foro com sem?foro para padestres                      *
  * Wendell Rocha, 115210754                                             *
- * Iniciação à Computação, 2015.2                                       *
+ * Inicia??o ? Computa??o, 2015.2                                       *
  * wendell.rocha@ccc.ufcg.edu.br                                        *
- * Feito por: Wendell Stanley Araújo Rocha                              *
- * Link para o repositório: https://github.com/WendellRocha/Arduino     *
- * Código desponibilizado para ser utilizado e alterado para se adequar *
- * ao seu código.                                                       *
- * Por favor, não retire os créditos.                                   *
+ * Feito por: Wendell Stanley Ara?jo Rocha                              *
+ * Link para o reposit?rio: https://github.com/WendellRocha/Arduino     *
+ * C?digo desponibilizado para ser utilizado e alterado para se adequar *
+ * ao seu c?digo.                                                       *
+ * Por favor, n?o retire os cr?ditos.                                   *
  ************************************************************************/ 
 
-const int botao = 8; // botão do semáforo no pino 8
+const int botao = 8; // bot?o do sem?foro no pino 8
 const int leds[] = {9, 10, 11, 12, 13}
-int estadodobotao = 0; // estado do botão do semáforo
+int estadodobotao = 0; // estado do bot?o do sem?foro
 
 /**
   led verde do pedestre no pino 9
@@ -26,46 +26,46 @@ void setup() {
    for(int i = 0; i < 4; i++) {
     pinMode(leds[i], OUTPUT);
   }
-  pinMode(botao, INPUT); // pino do botão em modo de entrada
+  pinMode(botao, INPUT); // pino do bot?o em modo de entrada
 
 }
 
 void loop() {
  
-  estadodobotao = digitalRead(botao); // atribui o valor da leitura do pino do botão ao estadodobotao
-  if (estadodobotao == HIGH) { // se o estado do botão for HIGH (alto, 1)
+  estadodobotao = digitalRead(botao); // atribui o valor da leitura do pino do bot?o ao estadodobotao
+  if (estadodobotao == HIGH) { // se o estado do bot?o for HIGH (alto, 1)
     delay(10000); // inicia uma contagem de 10 segundos    
-    digitalWrite(leds[3], LOW); // após a contagem de 10 segundos, apaga o led verde
-    digitalWrite(leds[2], HIGH); // após apagar o led vede, acende o amarelo
+    digitalWrite(leds[3], LOW); // ap?s a contagem de 10 segundos, apaga o led verde
+    digitalWrite(leds[2], HIGH); // ap?s apagar o led vede, acende o amarelo
     delay(5000); // inicia uma contagem de 5 segundos
-    digitalWrite(leds[2], LOW); // após a contagem de 5 segundos, apaga o led amarelo
-    digitalWrite(leds[4], LOW); // após a contagem de 5 segundos, apaga o led vermelho para o padestre
-    digitalWrite(led_red, HIGH); // após a contagem de 5 segundos, acende o led vermelho para os veículos
-    delay(750); // delay de segurança para acender o led verde para os pedestres
-    digitalWrite(leds[0], HIGH); // após a contagem de 75 milisegundos, acende o led verde para o pedestre
+    digitalWrite(leds[2], LOW); // ap?s a contagem de 5 segundos, apaga o led amarelo
+    digitalWrite(leds[4], LOW); // ap?s a contagem de 5 segundos, apaga o led vermelho para o padestre
+    digitalWrite(led_red, HIGH); // ap?s a contagem de 5 segundos, acende o led vermelho para os ve?culos
+    delay(750); // delay de seguran?a para acender o led verde para os pedestres
+    digitalWrite(leds[0], HIGH); // ap?s a contagem de 75 milisegundos, acende o led verde para o pedestre
     delay(15000); // inicia uma contagem de 15 segundos
 
-    for (int y = 0; y < 1; y++) { // para y igual a zero; y menor que 1; use y e depois incremente                    /* laço para piscar o led vermelho no final da contagem */
-      digitalWrite(leds[1], LOW); // apaga o led vermelho dos veículos
+    for (int y = 0; y < 1; y++) { // para y igual a zero; y menor que 1; use y e depois incremente                    /* la?o para piscar o led vermelho no final da contagem */
+      digitalWrite(leds[1], LOW); // apaga o led vermelho dos ve?culos
       delay(250); // conta 25 milisegundos
-      digitalWrite(leds[3], HIGH); // acende o led vermelho dos veículos
+      digitalWrite(leds[3], HIGH); // acende o led vermelho dos ve?culos
     }
        
-    for (int x = 0; x < 10; x++) { // para x igual a zero; x menor que dez; use x e depois incremete                    /* laço para piscar o led vermelho no final da contagem */
+    for (int x = 0; x < 10; x++) { // para x igual a zero; x menor que dez; use x e depois incremete                    /* la?o para piscar o led vermelho no final da contagem */
       digitalWrite(leds[4], HIGH); // acende o led verde do padestre
       delay(250); // conta 25 milisegundos
       digitalWrite(leds[4], LOW); // apaga o lede verde do padestre
       delay(250); // conta 2,5 segundos
     }
     
-    digitalWrite(leds[1], LOW); // após a contagem de 15 segundos, apaga o led vermelho
-    digitalWrite(leds[0], LOW); // após a contagem de 15 segundos, apaga o led verde do padestre
+    digitalWrite(leds[1], LOW); // ap?s a contagem de 15 segundos, apaga o led vermelho
+    digitalWrite(leds[0], LOW); // ap?s a contagem de 15 segundos, apaga o led verde do padestre
       
   }
-  else { // senão 
+  else { // sen?o 
     
-     digitalWrite(led_green, HIGH); // assim que o programa iniciar, ligará o led verde para os veículos
-     digitalWrite(led_red_ped, HIGH); // assim que o programa iniciar, ligará o led vermelho para o padestre
+     digitalWrite(led_green, HIGH); // assim que o programa iniciar, ligar? o led verde para os ve?culos
+     digitalWrite(led_red_ped, HIGH); // assim que o programa iniciar, ligar? o led vermelho para o padestre
   }
 
 }
--- semaforo_simples/semaforo_simples.ino
+++ semaforo_simples/semaforo_simples.ino
@@ -1,55 +1,55 @@
-/* Desafio do sem√°foro com sem√°foro para padestres                      *
+/* Desafio do sem·foro com sem·foro para padestres                      *
  * Wendell Rocha, 115210754                                             *
- * Inicia√ß√£o √† Computa√ß√£o, 2015.2                                       *
+ * IniciaÁ„o ‡ ComputaÁ„o, 2015.2                                       *
  * wendell.rocha@ccc.ufcg.edu.br                                        *
- * Feito por: Wendell Stanley Ara√∫jo Rocha                              *
- * Link para o reposit√≥rio: https://github.com/WendellRocha/Arduino     *
- * C√≥digo desponibilizado para ser utilizado e alterado para se adequar *
- * ao seu c√≥digo.                                                       *
- * Por favor, n√£o retire os cr√©ditos.                                   *
+ * Feito por: Wendell Stanley Ara˙jo Rocha                              *
+ * Link para o repositÛrio: https://github.com/WendellRocha/Arduino     *
+ * CÛdigo desponibilizado para ser utilizado e alterado para se adequar *
+ * ao seu cÛdigo.                                                       *
+ * Por favor, n„o retire os crÈditos.                                   *
  ************************************************************************/
 
-const int botao = 8; // bot√£o do sem√°foro no pino 8
+const int botao = 8; // bot„o do sem·foro no pino 8
 const int led_verde_pedestre = 9; // led verde do pedestre no pino 9
 const int led_vermelho = 10; // led vermelho no pino 10
 const int led_amarelo = 11; // led amarelo no pino 11
 const int led_verde = 12; // led verde no pino 12
 const int led_vermelho_pedestre = 13; // led vermelho do pedestre no pino 13
-int estadodobotao = 0; // estado do bot√£o do sem√°foro
+int estadodobotao = 0; // estado do bot„o do sem·foro
 
 
 void setup() {
-  pinMode(led_vermelho, OUTPUT); // pino do led_vermelho em modo de sa√≠da
-  pinMode(led_amarelo, OUTPUT); // pino do led_amarelo em modo de sa√≠da
-  pinMode(led_verde, OUTPUT); // pino do led_verde em modo de sa√≠da
-  pinMode(led_vermelho_pedestre, OUTPUT); // pino do led_vermelho_pedestre em modo de sa√≠da
-  pinMode(led_verde_pedestre, OUTPUT); // pino do led_verde_pedestre em modo de sa√≠da
-  pinMode(botao, INPUT); // pino do bot√£o em modo de entrada
+  pinMode(led_vermelho, OUTPUT); // pino do led_vermelho em modo de saÌda
+  pinMode(led_amarelo, OUTPUT); // pino do led_amarelo em modo de saÌda
+  pinMode(led_verde, OUTPUT); // pino do led_verde em modo de saÌda
+  pinMode(led_vermelho_pedestre, OUTPUT); // pino do led_vermelho_pedestre em modo de saÌda
+  pinMode(led_verde_pedestre, OUTPUT); // pino do led_verde_pedestre em modo de saÌda
+  pinMode(botao, INPUT); // pino do bot„o em modo de entrada
 
 }
 
 void loop() {
  
-  estadodobotao = digitalRead(botao); // atribui o valor da leitura do pino do bot√£o ao estadodobotao
-  if (estadodobotao == HIGH) { // se o estado do bot√£o for HIGH (alto, 1)
+  estadodobotao = digitalRead(botao); // atribui o valor da leitura do pino do bot„o ao estadodobotao
+  if (estadodobotao == HIGH) { // se o estado do bot„o for HIGH (alto, 1)
     delay(10000); // inicia uma contagem de 10 segundos    
-    digitalWrite(led_verde, LOW); // ap√≥s a contagem de 10 segundos, apaga o led verde
-    digitalWrite(led_amarelo, HIGH); // ap√≥s apagar o led vede, acende o amarelo
+    digitalWrite(led_verde, LOW); // apÛs a contagem de 10 segundos, apaga o led verde
+    digitalWrite(led_amarelo, HIGH); // apÛs apagar o led vede, acende o amarelo
     delay(5000); // inicia uma contagem de 5 segundos
-    digitalWrite(led_amarelo, LOW); // ap√≥s a contagem de 5 segundos, apaga o led amarelo
-    digitalWrite(led_vermelho_pedestre, LOW); // ap√≥s a contagem de 5 segundos, apaga o led vermelho para o padestre
-    digitalWrite(led_vermelho, HIGH); // ap√≥s a contagem de 5 segundos, acende o led vermelho para os ve√≠culos
-    delay(750); // delay de seguran√ßa para acender o led verde para os pedestres
-    digitalWrite(led_verde_pedestre, HIGH); // ap√≥s a contagem de 75 milisegundos, acende o led verde para o pedestre
+    digitalWrite(led_amarelo, LOW); // apÛs a contagem de 5 segundos, apaga o led amarelo
+    digitalWrite(led_vermelho_pedestre, LOW); // apÛs a contagem de 5 segundos, apaga o led vermelho para o padestre
+    digitalWrite(led_vermelho, HIGH); // apÛs a contagem de 5 segundos, acende o led vermelho para os veÌculos
+    delay(750); // delay de seguranÁa para acender o led verde para os pedestres
+    digitalWrite(led_verde_pedestre, HIGH); // apÛs a contagem de 75 milisegundos, acende o led verde para o pedestre
     delay(15000); // inicia uma contagem de 15 segundos
-    digitalWrite(led_vermelho, LOW); // ap√≥s a contagem de 15 segundos, apaga o led vermelho
-    digitalWrite(led_verde_pedestre, LOW); // ap√≥s a contagem de 15 segundos, apaga o led verde do padestre
+    digitalWrite(led_vermelho, LOW); // apÛs a contagem de 15 segundos, apaga o led vermelho
+    digitalWrite(led_verde_pedestre, LOW); // apÛs a contagem de 15 segundos, apaga o led verde do padestre
       
   }
-  else { // sen√£o 
+  else { // sen„o 
     
-     digitalWrite(led_verde, HIGH); // assim que o programa iniciar, ligar√° o led verde para os ve√≠culos
-     digitalWrite(led_vermelho_pedestre, HIGH); // assim que o programa iniciar, ligar√° o led vermelho para o padestre
+     digitalWrite(led_verde, HIGH); // assim que o programa iniciar, ligar· o led verde para os veÌculos
+     digitalWrite(led_vermelho_pedestre, HIGH); // assim que o programa iniciar, ligar· o led vermelho para o padestre
   }
 
 }
