bafb7fe9c476b499c3843b7a5758b2e16afe6fb7
Wendell Rocha <wendellrocha@outlook.com>
2016-10-17 15:32:40 -0300

    arduino_java addeded

---
+++ arduino_java/src/arduino_java/SerialTest.java
@@ -0,0 +1,98 @@
+/*
+ * Codigo retirado do Arduino playground
+ * http://playground.arduino.cc/Interfacing/Java
+ */
+
+package arduino_java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier; 
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent; 
+import gnu.io.SerialPortEventListener; 
+import java.util.Enumeration;
+
+
+public class SerialTest implements SerialPortEventListener {
+	SerialPort serialPort;
+	private static final String PORT_NAMES[] =  {
+			"/dev/tty.usbserial-A9007UX1", // Mac OS X
+            "/dev/ttyACM0", // Raspberry Pi
+            "/dev/ttyUSB0", // Linux
+            "COM3", // Windows};
+	};
+	private BufferedReader input;
+	private OutputStream output;
+	private static final int TIME_OUT = 2000;
+	private static final int DATA_RATE = 9600;
+
+	public void initialize() {
+                System.setProperty("gnu.io.rxtx.SerialPorts", "COM3");
+
+		CommPortIdentifier portId = null;
+		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+		while (portEnum.hasMoreElements()) {
+			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+			for (String portName : PORT_NAMES) {
+				if (currPortId.getName().equals(portName)) {
+					portId = currPortId;
+					break;
+				}
+			}
+		}
+		if (portId == null) {
+			System.out.println("Nao foi possivel encontrar a porta COM.");
+			return;
+		}
+
+		try {
+			serialPort = (SerialPort) portId.open(this.getClass().getName(),
+					TIME_OUT);
+
+			serialPort.setSerialPortParams(DATA_RATE,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+
+			input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+			output = serialPort.getOutputStream();
+
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+	}
+
+	public synchronized void close() {
+		if (serialPort != null) {
+			serialPort.removeEventListener();
+			serialPort.close();
+		}
+	}
+
+	public synchronized void serialEvent(SerialPortEvent oEvent) {
+		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			try {
+				String inputLine=input.readLine();
+				System.out.println(inputLine);
+			} catch (Exception e) {
+				System.err.println(e.toString());
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		SerialTest main = new SerialTest();
+		main.initialize();
+		Thread t=new Thread() {
+			public void run() {
+				try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+			}
+		};
+		t.start();
+		System.out.println("Started");
+	}
+}
